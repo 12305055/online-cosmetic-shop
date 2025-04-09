106d1aed2987268f8ff92d49f9f4629b5f914100
ambuj chaubey <ambuj12305055lpu@gmail.com>
2025-04-09 15:49:53 +0530

    sms

--- script.js
+++ script.js
@@ -61,5 +61,9 @@ buyNowButton.onclick = () => {
       alert("Your cart is empty.");
       return;
   }
+  else{
+    alert("your cart is not empty.");
+  }
   window.location.href = "location.html"; 
+ 
 };
